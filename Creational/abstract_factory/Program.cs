1c9ac0a383d84a16f0e19b5a69b0532de169acf6
Gato-Capitao <adsongabrielsilva14@gmail.com>
2024-01-08 05:35:51 -0300

    Modificações na estrutura do arquivo.

--- Creational/abstract_factory/Program.cs
+++ Creational/abstract_factory/Program.cs
@@ -19,52 +19,71 @@ namespace DesignPatterns.AbstractFactory
 
     public class ConcreteFactory2 : AbstractFactory{
         public IAbstractProductA createProductA(){
-            return new ConcreteProductA2()
+            return new ConcreteProductA2();
         }
 
         public IAbstractProductB createProductB(){
-            return new concreteProductB2()
+            return new concreteProductB2();
         }
     }
 
     public interface IAbstractProductA{
-        string usefulFunctionA()
+        string usefulFunctionA();
     }
 
     class ConcreteProductA1 : IAbstractProductA{
         public string usefulFunctionA(){
-            return "Função de A1"
+            return "Função de A1";
         }
     }
 
     class ConcreteProductA2 : IAbstractProductA{
         public string usefulFunctionA(){
-            return "Função de A2"
+            return "Função de A2";
         }
     }
 
     public interface IAbstractProductB{
-        string usefulFunctionB()
+        string usefulFunctionB();
     }
 
     class ConcreteProductB1 : IAbstractProductB{
         public string usefulFunctionB(){
-            return "Função útil de B1"
+            return "Função útil de B1";
         }
     }
 
     class concreteProductB2 : IAbstractProductB{
         public string usefulFunctionB(){
-            return "Função útil de B2"
+            return "Função útil de B2";
         }
     }
 
+    class Client{
+        public void Main(){
+            Console.WriteLine("Testing first factory.");
+            ClientMethod(new ConcreteFactory1());
+
+            Console.WriteLine("Testing second factory.");
+            ClientMethod(new ConcreteFactory2());
+        }
+        public void ClientMethod(AbstractFactory factory){
+            //Creating products
+            IAbstractProductA productA = factory.createProductA();
+            IAbstractProductB productB = factory.createProductB();
+
+            //Testing
+            Console.WriteLine(productA.usefulFunctionA());
+            Console.WriteLine(productB.usefulFunctionB());
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {   
-            Console.WriteLine("oi");
+            Client client = new Client()
+            client.Main()
         }
     }
 }
