f2ae657308c74a64ee47721e3ea5eedd6a1dc6fb
Gato-Capitao <adsongabrielsilva14@gmail.com>
2024-01-10 11:57:07 -0300

    testando funcionalidade de Julia

--- Behavioral/observer.jl
+++ Behavioral/observer.jl
@@ -1,1 +1,12 @@
-print("hello")
+struct Point
+    x::Float64
+    y::Float64
+end
+function distance(p1::Point, p2::Point)
+    return sqrt((p1.x - p2.x)^2 + (p1.y - p2.y)^2)
+end
+
+p1 = Point(1.0, 2.0)
+p2 = Point(3.0, 4.0)
+
+println(distance(p1, p2))
