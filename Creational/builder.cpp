2c1b44099bba4bd1c1ab48838e541b4b94889a63
Adson <adsongabrielfsilva@gmail.com>
2024-02-08 10:44:17 -0300

    Criando classe carro

--- Creational/builder.cpp
+++ Creational/builder.cpp
@@ -1,5 +1,40 @@
-#include<stdio.h>
+#include<string>
+#include<iostream>
+using namespace std;
+
+class Car{
+public:
+    int numberDoors, numberWheels;
+    string name, engine, color;
+
+    void showInformation(){
+        cout << "Name: " << name << "\n";
+        cout << "Color: " << color << "\n";
+        cout << "Engine: " << engine << "\n";
+        cout << "Number wheels: " << numberWheels << "\n";
+        cout << "Number doors: " << numberDoors << "\n";
+    }
+};
+
+class ConstructCar{
+public:
+    void getName(){
+        return "noName";
+    }
+
+    void getColor(){
+        return "White";
+    }
+}
+
 int main(){
-    printf("Hello, world!");
+    Car car;
+    car.name = "Storm";
+    car.color = "Red";
+    car.engine = "V8";
+    car.numberDoors = 2;
+    car.numberWheels = 4;
+    
+    car.showInformation();
     return 0;
 }
