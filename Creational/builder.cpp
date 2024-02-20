925add3e5c46f8e0dec74604629a135c92ec4c4f
Adson <adsongabrielfsilva@gmail.com>
2024-02-20 15:24:42 -0300

    Finalizando builder

--- Creational/builder.cpp
+++ Creational/builder.cpp
@@ -16,25 +16,112 @@ public:
     }
 };
 
-class ConstructCar{
+class IConstructCar{
 public:
-    void getName(){
-        return "noName";
+    virtual string setName() = 0;
+    virtual string setColor() = 0;
+    virtual string setEngine() = 0;
+    virtual int setNumberWheels() = 0;
+    virtual int setNumberDoors() = 0;
+};
+
+class ConcreteConstructorFord: public IConstructCar{
+public:
+    string setName() override{
+        return "Ford Mustang GT 1967";
     }
 
-    void getColor(){
+    string setColor() override{
+        return "Sky blue";
+    }
+
+    string setEngine() override{
+        return "Ford 351 Windsor V8";
+    }
+
+    int setNumberDoors() override{
+        return 2;
+    }
+
+    int setNumberWheels() override{
+        return 4;
+    }
+};
+
+class ConcreteConstructorFiat: public IConstructCar{
+public:
+    string setName() override{
+        return "Fiat 500 2008";
+    }
+
+    string setColor() override{
         return "White";
     }
-}
+
+    string setEngine() override{
+        return "Fiat TwinAir 0.9L";
+    }
+
+    int setNumberDoors() override{
+        return 2;
+    }
+
+    int setNumberWheels() override{
+        return 4;
+    }
+};
+
+class ConcreteConstructorTesla: public IConstructCar{
+public:
+    string setName() override{
+        return "Tesla Model 3 2023";
+    }
+
+    string setColor() override{
+        return "Black";
+    }
+
+    string setEngine() override{
+        return "Tesla Electric Motor";
+    }
+
+    int setNumberDoors() override{
+        return 4;
+    }
+
+    int setNumberWheels() override{
+        return 4;
+    }
+};
+
+class Director{
+public:
+    Car buildCar(IConstructCar& constructor){
+        Car car;
+        car.name = constructor.setName();
+        car.engine = constructor.setEngine();
+        car.color = constructor.setColor();
+        car.numberDoors = constructor.setNumberDoors();
+        car.numberWheels = constructor.setNumberWheels();
+
+        return car;
+    }
+};
 
 int main(){
-    Car car;
-    car.name = "Storm";
-    car.color = "Red";
-    car.engine = "V8";
-    car.numberDoors = 2;
-    car.numberWheels = 4;
-    
+    Director director;
+    ConcreteConstructorFord constructorFord;
+    ConcreteConstructorFiat constructorFiat;
+    ConcreteConstructorTesla constructorTesla;
+
+    Car car = director.buildCar(constructorFord);
     car.showInformation();
-    return 0;
+
+    cout << "\n";
+    car = director.buildCar(constructorFiat);
+    car.showInformation();
+
+    cout << "\n";
+    car = director.buildCar(constructorTesla);
+    car.showInformation();
 }
